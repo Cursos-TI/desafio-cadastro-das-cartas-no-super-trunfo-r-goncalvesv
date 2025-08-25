0361c6722a0d3ba0a83398cd9e89f3b72649b08d
Rodrigo Gonçalves <rodrigoliveiraedf@gmail.com>
2025-08-25 17:56:00 -0300

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,11 +1,32 @@
 int main(){
-  char estado; [2] = Brasilia, Fortaleza;
+  char estado; 
   char codigo da carta [04]; 
   char nome da cidade [50];
   int populaçao;
   float area;
   float pib;
   int numero de pontos turisticos;
+
+printf ("Digite o estado:\n");
+scanf("%d", &estado);
+
+printf ("Digite o codigo da carta:\n");
+scanf("%cd", &codigo da carta);
+
+printf("Digite o nome da cidade:\n");
+scanf("%n", &nome da cidade);
+
+printf("Digite a populaçao:\n");
+scanf("%n", &populacao);
+
+printf("Insira a area:\n");
+scanf("%2.a", &area);
+
+printf("Insira o pib:\n");
+scanf("%p", &pib);
+
+printf("Insira o numero de pontos turisticos:"\n);
+scanf("%t", &numero de pontos turisticos);
 
 
 
