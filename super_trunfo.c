dd42f6ea6028922e2adf128ce113b2faae5f8a66
Rodrigo Gonçalves <rodrigoliveiraedf@gmail.com>
2025-08-25 17:43:48 -0300

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,1 +1,12 @@
-int main
+int main(){
+  char estado; [2] = Brasilia, Fortaleza;
+  char codigo da carta [04]; 
+  char nome da cidade [50];
+  int populaçao;
+  float area;
+  float pib;
+  int numero de pontos turisticos;
+
+
+
+
